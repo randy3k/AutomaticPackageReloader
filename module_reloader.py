d4a3a1c98f9c1f4c380620dc98abc6e13b94f068
Randy Lai <randy.cs.lai@gmail.com>
2016-07-22 02:07:12 -0400

    use _module_reloader.py to trick sublime to reload plugins reload all python files at the top level

--- module_reloader.py
+++ module_reloader.py
@@ -10,14 +10,6 @@ import os
 
 
 def trace(*args, tag="debug", fill=None, fill_width=60, **kwargs):
-    """
-    Lightweight logging facility. Provides simple print-like interface with
-    filtering by tags and pretty-printed captions for delimiting output
-    sections.
-
-    See the "dev_trace" setting for possible values of the "tag" keyword.
-    """
-
     if fill is not None:
         sep = str(kwargs.get('sep', ' '))
         caption = sep.join(args)
@@ -81,21 +73,16 @@ class ModuleReloaderReloadCommand(sublime_plugin.WindowCommand):
             sublime.set_timeout_async(lambda: reload_package(pkg_name))
 
 
+# check the link for comments
 # https://github.com/divmain/GitSavvy/blob/599ba3cdb539875568a96a53fafb033b01708a67/common/util/reload.py
 
 def reload_package(pkg_name):
     if pkg_name not in sys.modules:
-        dprint("ERROR", pkg_name, "is not loaded.")
+        dprint("ERROR:", pkg_name, "is not loaded.")
         return
 
-    # we need to disable and reenable the package enable to make sure
-    # the TextCommand's and WindowCommand's would be loaded.
-    psettings = sublime.load_settings("Preferences.sublime-settings")
-    ignored_packages = psettings.get("ignored_packages", [])
-    ignored_packages.append(pkg_name)
-    psettings.set("ignored_packages", ignored_packages)
-
     main = sys.modules[pkg_name]
+
     dprint("begin", fill='=')
 
     modules = {name: module for name, module in sys.modules.items()
@@ -111,57 +98,32 @@ def reload_package(pkg_name):
 
     dprint("end", fill='-')
 
-    ignored_packages.pop(-1)
-    psettings.set("ignored_packages", ignored_packages)
-
+    # a hack to trigger automatic "reloading plugins"
+    # this is needed to ensure TextCommand's and WindowCommand's are ready.
+    dummy = os.path.join(sublime.packages_path(), "_moduler_reloader.py")
+    open(dummy, "w").close()
+    sublime.set_timeout(lambda: os.path.exists(dummy) and os.unlink(dummy), 100)
     sublime.set_timeout(lambda: sublime.status_message("Module Reloaded."), 500)
 
 
 def ensure_loaded(main, modules):
-    # More simple (comparing to reload_modules(perform_reload=False)) and dumb
-    # approach to ensure all modules are back. Quite useful when debugging the
-    # "reload" module itself, i.e. for cases when reloading might fail due to
-    # bugs in reload_modules().
     missing_modules = {name: module for name, module in modules.items()
                        if name not in sys.modules}
     if missing_modules:
         for name, module in missing_modules:
             sys.modules[name] = modules
-            dprint("Error", "Bug!", "restored", name)
-        sublime_plugin.reload_plugin(main.__name__)
+            dprint("Error:", "Bug!", "restored", name)
+        reload_plugin(main.__name__)
 
 
 def reload_modules(main, modules, perform_reload=True):
-    """Implements the machinery for reloading a given plugin module."""
-    #
-    # Here's the approach in general:
-    #
-    #   - Hide GitSavvy modules from the sys.modules temporarily;
-    #
-    #   - Install a special import hook onto sys.meta_path;
-    #
-    #   - Call sublime_plugin.reload_plugin(), which imports the main
-    #     "git_savvy" module under the hood, triggering the hook;
-    #
-    #   - The hook, instead of creating a new module object, peeks the saved
-    #     one and reloads it. Once the module encounters an import statement
-    #     requesting another module, not yet reloaded, the hook reenters and
-    #     processes that new module recursively, then get back to the previous
-    #     one, and so on.
-    #
-    # This makes the modules reload in the very same order as they were loaded
-    # initially, as if they were imported from scratch.
-    #
+
     if perform_reload:
         sublime_plugin.unload_module(main)
 
-    # Insert the main "git_savvy" module at the beginning to make the reload
-    # order be as close to the order of the "natural" import as possible.
     module_names = [main.__name__] + sorted(name for name in modules
                                             if name != main.__name__)
 
-    # First, remove all the loaded modules from the sys.modules cache,
-    # otherwise the reloading hook won't be called.
     loaded_modules = dict(sys.modules)
     for name in loaded_modules:
         if name in modules:
@@ -185,22 +147,24 @@ def reload_modules(main, modules, perform_reload=True):
                     raise
         else:
             if name not in loaded_modules:
-                dprint("NO RELOAD", '---', name)
+                dprint("No Reload", '---', name)
             return module
 
     with intercepting_imports(module_reloader), importing_fromlist_aggresively(modules):
-        # Now, import all the modules back, in order, starting with the main
-        # module. This will reload all the modules directly or indirectly
-        # referenced by the main one, i.e. usually most of our modules.
-        sublime_plugin.reload_plugin(main.__name__)
-
-        # Be sure to bring back *all* the modules that used to be loaded, not
-        # only these imported through the main one. Otherwise, some of them
-        # might end up being created from scratch as new module objects in
-        # case of being imported after detaching the hook. In general, most of
-        # the imports below (if not all) are no-ops though.
+        reload_plugin(main.__name__)
         for name in module_names:
             importlib.import_module(name)
+
+
+def reload_plugin(pkg_name):
+    pkg_path = os.path.join(os.path.realpath(sublime.packages_path()), pkg_name)
+    if os.path.exists(os.path.join(pkg_path, "__init__.py")):
+        sublime_plugin.reload_plugin(pkg_name)
+    else:
+        plugins = [pkg_name + "." + os.path.splitext(f)[0]
+                   for f in os.listdir(pkg_path) if f.endswith(".py")]
+        for plugin in plugins:
+            sublime_plugin.reload_plugin(plugin)
 
 
 @contextmanager
@@ -209,58 +173,13 @@ def importing_fromlist_aggresively(modules):
 
     @functools.wraps(orig___import__)
     def __import__(name, globals=None, locals=None, fromlist=(), level=0):
-        # Given an import statement like this:
-        #
-        #     from .some.module import something
-        #
-        # The original __import__ performs roughly the following steps:
-        #
-        #   - Import ".some.module", just like the importlib.import_module()
-        #     function would do, i.e. resolve packages, calculate the absolute
-        #     name, check sys.modules for that module, invoke import hooks and
-        #     so on...
-        #
-        #   - For each name specified in the "fromlist" (a "something" in our
-        #     case), ensure the module have that name in its namespace. This
-        #     could be:
-        #
-        #       - a regular name defined within that module, like a function
-        #         named "something", and in this case we're done;
-        #
-        #       - or, in case the module is missing that attribute, there's a
-        #         chance that the requested name refers to a submodule of that
-        #         module, ".some.module.something", and we need to import it.
-        #         Once imported it will take care to register itself within
-        #         the parent's namespace.
-        #
-        # This looks natural and it is indeed in case of loading a module for
-        # the first time. But things start to behave slightly different once
-        # you try to reload a module.
-        #
-        # The main difference is that during the reload the module code is
-        # executed with its dictionary retained. And this has an undesired
-        # effect on handling the "fromlist" as described above: the second
-        # part (involving import of a submodule) is only executed when the
-        # module dictionary is missing the submodule name, which is not the
-        # case during the reload.
-        #
-        # This is generally not a problem: the name refers to the submodule
-        # imported earlier anyway. But we need to import it in order to force
-        # the necessary hook to reload that submodule too.
-
         module = orig___import__(name, globals, locals, fromlist, level)
         if fromlist and module.__name__ in modules:
-            # Refer to _handle_fromlist() from "importlib/_bootstrap.py"
             if '*' in fromlist:
                 fromlist = list(fromlist)
                 fromlist.remove('*')
                 fromlist.extend(getattr(module, '__all__', []))
             for x in fromlist:
-                # Here's an altered part of logic.
-                #
-                # The original __import__ doesn't even try to import a
-                # submodule if its name is already in the module namespace,
-                # but we do that for certain set of the known submodule.
                 if isinstance(getattr(module, x, None), types.ModuleType):
                     from_name = '{}.{}'.format(module.__name__, x)
                     if from_name in modules:
