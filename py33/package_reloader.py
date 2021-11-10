fbf53deee4ce411de42c46e9a5f15f5d179db6c9
Randy Lai <randy.cs.lai@gmail.com>
2021-11-10 15:35:02 -0800

    no longer need to use importlib

--- py33/package_reloader.py
+++ py33/package_reloader.py
@@ -1,11 +1,3 @@
-import sys
-from importlib.machinery import PathFinder
-
-
-loader = PathFinder.find_module("AutomaticPackageReloader")
-mod = loader.load_module("AutomaticPackageReloader")
-sys.modules["AutomaticPackageReloader"] = mod
-
 from AutomaticPackageReloader import package_reloader as package_reloader38  # noqa
 
 
